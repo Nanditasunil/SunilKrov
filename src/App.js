fccac7e2e0fac8ce5383daf996880d79588d18f4
Nanditasunil <109030620+Nanditasunil@users.noreply.github.com>
2024-05-08 11:44:33 +0530

    initial draft of navbar and hero

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./component/NavBar";
+import Hero from "./component/Hero";
 
 function App() {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
